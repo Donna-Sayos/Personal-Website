6ba4a12d13f6aa18bc645011a6708302c9d46ebb
Donna-Sayos <Donnasayos@gmail.com>
2022-09-15 01:46:38 -0400

    added portfolios

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,8 +4,8 @@ import { useState } from "react";
 
 import { TiHomeOutline } from "react-icons/ti";
 import { FaRegUser } from "react-icons/fa";
-import { BiBookBookmark, BiMessageDetail } from "react-icons/bi";
-import { RiServiceLine } from "react-icons/ri";
+import { MdWorkOutline } from "react-icons/md";
+import { BiBookBookmark, BiMessageDetail, BiHappyBeaming } from "react-icons/bi";
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState("#");
@@ -14,7 +14,8 @@ const Nav = () => {
       <a href="/#" onClick={() => setActiveNav("#")} className={activeNav === "#" ? "active" : ""}><TiHomeOutline/></a>
       <a href="#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? "active" : ""}><FaRegUser/></a>
       <a href="#experience" onClick={() => setActiveNav("#experience")} className={activeNav === "#experience" ? "active" : ""}><BiBookBookmark/></a>
-      <a href="#services" onClick={() => setActiveNav("#services")} className={activeNav === "#services" ? "active" : ""}><RiServiceLine/></a>
+      <a href="#portfolio" onClick={() => setActiveNav("#portfolio")} className={activeNav === "#portfolio" ? "active" : ""}><MdWorkOutline/></a>
+      <a href="#testimonials" onClick={() => setActiveNav("#testimonials")} className={activeNav === "#testimonials" ? "active" : ""}><BiHappyBeaming/></a>
       <a href="#contact" onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? "active" : ""}><BiMessageDetail/></a>
     </nav>
   )
